407fc9827bad823a379af2841a68c0b8123e6f81
Jchino15 <99441203+Jchino15@users.noreply.github.com>
2022-06-05 18:08:57 -0600

    Add files via upload

---
+++ src/colegio/Colegio.java
@@ -0,0 +1,14 @@
+package colegio;
+
+public class Colegio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmPrincipal frm = new FrmPrincipal();
+        frm.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
